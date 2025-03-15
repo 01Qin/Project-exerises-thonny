3e38a7745fca51384468a4db4283421dc2f9cf44
01Qin <sisukasqin@hotmail.com>
2025-03-15 14:42:10 +0200

    exercises 5

---
+++ mqtt_publish_test.py
@@ -0,0 +1,25 @@
+import network
+from time import sleep
+
+# Replace these values with your own
+SSID = "Xiaomi_C24F"
+PASSWORD = "kele453489."
+BROKER_IP = "192.168.31.1"
+
+# Function to connect to WLAN
+def connect_wlan():
+    # Connecting to the group WLAN
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(SSID, PASSWORD)
+
+    # Attempt to connect once per second
+    while wlan.isconnected() == False:
+        print("Connecting... ")
+        sleep(1)
+
+    # Print the IP address of the Pico
+    print("Connection successful. Pico IP:", wlan.ifconfig()[0])
+
+# Main program
+connect_wlan()
